e510908beb413dcff9e089e14df9341b7a8b3fee
Natal <96358562+Natalmaionese@users.noreply.github.com>
2025-02-28 12:31:43 +0100

    Update fa.sv

--- src/fa.sv
+++ src/fa.sv
@@ -1,21 +1,23 @@
 //  Module: fa
 //
 
-module fa_module
-    (
-        input logic A,
-        input logic B,
-        input logic Cin,
-        output logic S,
-        output logic Cout
-    );
+module fa_module (
+    input logic A,
+    input logic B,
+    input logic Cin,
+    output logic S,
+    output logic Cout
+);
 
     logic i0, i1, i2;
 
-    xor u0(i0, A, B);
+    xor u0(i0, A, B);     
+    xor u1(S, i0, Cin);  
 
-    and u2(i1, A, B);
+    and u2(i1, A, B);     
+    and u3(i2, i0, Cin);  
 
-    or u4(Cout, i1, i2);
+    or  u4(Cout, i1, i2); 
 
 endmodule: fa_module
+
